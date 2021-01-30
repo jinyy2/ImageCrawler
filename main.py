f594dccce696ebbdfd17fd48554a9630430ee311
jinyy2 <jy5031le@gmail.com>
2021-01-30 19:40:58 +0900

    Organized file contents

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import urllib.request
 import requests
 import os
@@ -7,32 +8,13 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 import tkinter.font as tkFont
-from bs4 import BeautifulSoup
 
-#tkinter Setting
-app = Tk()
-app.title("GUI Crawler v1.0") # 창 타이틀
-app.geometry("300x200+800+400") #창 크기
-font = tkFont.Font(family="Consolas", size=16, weight="bold")
-
-label = Label(app, text="URL을 입력하세요.", font=font)
-label.pack(pady=30)
-
-#URL 입력창
-ent = Entry(app,width=30)
-ent.pack()
 
 def select_folder():
     global saveDir
     saveDir = filedialog.askdirectory() + "/"+ now + "/"
     print("1"+saveDir)
     pass
-
-now = datetime.datetime.now().strftime('%y%m%d_%H%M')
-saveDir = "C:/Images/" + now + "/"
-
-startBtn = Button(app, text = "Folder", width=10, command=select_folder)
-startBtn.pack(padx=10,pady=10)
 
 #크롤링 & 이미지 다운
 def crawling(url):
@@ -65,20 +47,35 @@ def start_button():
     print(url)
     if url is not "":
         crawling(url)
-
     else:
         messagebox.showinfo("Alert", "값을 입력해주세요.")
-
 
 def clear_button():
     ent.delete("0", "end")
 
-startBtn = Button(app, text = "Start", width=10, command=start_button)
-startBtn.pack(side='left',padx=30,pady=10)
+if __name__ == '__main__':
+    # tkinter Setting
+    app = Tk()
+    app.title("GUI Crawler v1.0")  # 창 타이틀
+    app.geometry("300x200+800+400")  # 창 크기
+    font = tkFont.Font(family="Consolas", size=16, weight="bold")
 
+    label = Label(app, text="URL을 입력하세요.", font=font)
+    label.pack(pady=30)
 
+    # URL 입력창
+    ent = Entry(app, width=30)
+    ent.pack()
 
-clearBtn = Button(app, text = "Clear", width=10, command=clear_button)
-clearBtn.pack(side='right',padx=30,pady=10)
-app.mainloop()
+    now = datetime.datetime.now().strftime('%y%m%d_%H%M')
+    saveDir = "C:/Images/" + now + "/"
 
+    startBtn = Button(app, text="Folder", width=10, command=select_folder)
+    startBtn.pack(padx=10, pady=10)
+
+    startBtn = Button(app, text="Start", width=10, command=start_button)
+    startBtn.pack(side='left', padx=30, pady=10)
+
+    clearBtn = Button(app, text="Clear", width=10, command=clear_button)
+    clearBtn.pack(side='right', padx=30, pady=10)
+    app.mainloop()
